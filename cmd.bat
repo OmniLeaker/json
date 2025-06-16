e488debeb2dc63d13a23a967f49fafdb23fc2680
omnileaker p2p <zajtprvi@gmail.com>
2025-06-16 18:40:36 +0200

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -2,8 +2,6 @@
 REM This batch script launches a new instance of the Command Prompt (cmd.exe).
 
 REM The 'start' command is used to launch programs or commands in a new window.
-REM /B     : Starts application without creating a new window. (Not used here)
-REM ""     : The first quoted string is the title for the new window. It's empty here.
 REM cmd.exe: The executable to launch.
 start "" cmd.exe
 
@@ -12,5 +10,5 @@ REM (if you run it by double-clicking) to stay open briefly before closing.
 REM If you run it from an existing cmd window, this line won't affect that window.
 REM pause
 
-REM The script will exit immediately after launching cmd.exe.
+REM exit after launching cmd.exe.
 exit
